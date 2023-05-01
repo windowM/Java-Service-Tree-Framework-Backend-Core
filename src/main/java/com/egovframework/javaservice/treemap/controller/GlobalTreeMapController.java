2222a01401194e1ff57d895c3162fe3a22858b5c
LeeDongMin <admin@313.co.kr>
2023-05-01 11:27:14 +0900

    [SP-313] #comment 글로벌 트리맵 :: 제품(서비스) - 버전 - 지라프로젝트 연결 23.05.01 #close #time 1h +review SR @dongmin.lee
    [ Backend-Core :: PLE-TreeFramework :: 313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- src/main/java/com/egovframework/javaservice/treemap/controller/GlobalTreeMapController.java
+++ src/main/java/com/egovframework/javaservice/treemap/controller/GlobalTreeMapController.java
@@ -1,9 +1,13 @@
 package com.egovframework.javaservice.treemap.controller;
 
 import com.egovframework.javaservice.treeframework.controller.CommonResponse;
+import com.egovframework.javaservice.treeframework.util.ParameterParser;
+import com.egovframework.javaservice.treeframework.util.StringUtility;
+import com.egovframework.javaservice.treeframework.util.StringUtils;
 import com.egovframework.javaservice.treemap.model.GlobalTreeMapEntity;
 import com.egovframework.javaservice.treemap.service.GlobalTreeMapService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
@@ -13,11 +17,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/arms/globaltreemap")
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class GlobalTreeMapController {
 
     private final GlobalTreeMapService globalTreeMapService;
@@ -71,6 +79,64 @@ public class GlobalTreeMapController {
         }
 
         return ResponseEntity.ok(CommonResponse.success(globalTreeMapService.delete(globalTreeMapEntityList.get(0))));
+
+    }
+
+    @ResponseBody
+    @RequestMapping(
+            value = {"/getConnectInfo/pdService/pdServiceVersion/jiraProject.do"},
+            method = {RequestMethod.GET}
+    )
+    public ResponseEntity<?> getConnectInfo_pdService_pdServiceVersion_jiraProject(GlobalTreeMapEntity globalTreeMapEntity, ModelMap model, HttpServletRequest request) throws Exception {
+
+        List<GlobalTreeMapEntity> savedList = globalTreeMapService.findAllBy(globalTreeMapEntity);
+
+        List<GlobalTreeMapEntity> filteredList = savedList.stream().filter(savedData ->
+                savedData.getPdservice_link() != null &&
+                        savedData.getPdserviceversion_link() != null &&
+                        savedData.getJiraproject_link() != null
+        ).collect(Collectors.toList());
+        return ResponseEntity.ok(CommonResponse.success(filteredList));
+
+    }
+
+    @ResponseBody
+    @RequestMapping(
+            value = {"/setConnectInfo/pdService/pdServiceVersion/jiraProject.do"},
+            method = {RequestMethod.POST}
+    )
+    public ResponseEntity<?> setConnectInfo_pdService_pdServiceVersion_jiraProject(GlobalTreeMapEntity globalTreeMapEntity, ModelMap model, HttpServletRequest request) throws Exception {
+
+        ParameterParser parser = new ParameterParser(request);
+        String[] jiraProjectList = StringUtility.jsonStringifyConvert(parser.get("c_pdservice_jira_ids"));
+
+        List<GlobalTreeMapEntity> saveTargetList = new ArrayList<>();
+
+        for( String jiraProject : jiraProjectList ){
+
+            GlobalTreeMapEntity saveTarget = new GlobalTreeMapEntity();
+            saveTarget.setPdservice_link(globalTreeMapEntity.getPdservice_link());
+            saveTarget.setPdserviceversion_link(globalTreeMapEntity.getPdserviceversion_link());
+            saveTarget.setJiraproject_link(Long.parseLong(jiraProject));
+
+            List<GlobalTreeMapEntity> checkList = globalTreeMapService.findAllBy(saveTarget);
+            List<GlobalTreeMapEntity> checkDuplicate = checkList.stream().filter(data ->
+                    data.getPdservice_link() == saveTarget.getPdservice_link() &&
+                            data.getPdserviceversion_link() == saveTarget.getPdserviceversion_link() &&
+                            data.getJiraproject_link() == saveTarget.getJiraproject_link()
+            ).collect(Collectors.toList());
+
+            log.info("===============================================================");
+
+
+            if( checkDuplicate == null || checkDuplicate.isEmpty() == true) {
+                globalTreeMapService.save(saveTarget);
+            }else{
+                log.info(globalTreeMapEntity.toString());
+            }
+        }
+
+        return ResponseEntity.ok(CommonResponse.success("test"));
 
     }
 
